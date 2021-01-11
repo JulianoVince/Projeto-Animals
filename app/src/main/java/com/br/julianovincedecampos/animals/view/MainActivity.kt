a2c291016f1f0bbfe551d44e002990dfc3852465
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-01-11 12:57:58 -0300

    Criando projeto, navigation e fragments

---
+++ app/src/main/java/com/br/julianovincedecampos/animals/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.br.julianovincedecampos.animals.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.julianovincedecampos.animals.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
